a8744dbd11f4bf7be48f3fbc51d82498bcdd581d
younghee <hn05035@naver.com>
2024-01-29 17:34:02 +0900

    sk

--- finalProject/src/main/java/com/bongsamaru/member/service/impl/MemberServiceImpl.java
+++ finalProject/src/main/java/com/bongsamaru/member/service/impl/MemberServiceImpl.java
@@ -1,34 +1,34 @@
-package com.bongsamaru.member.service.impl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import com.bongsamaru.member.MemberVO;
-import com.bongsamaru.member.UserDetailVO;
-import com.bongsamaru.member.mapper.MemberMapper;
-import com.bongsamaru.member.service.MemberService;
-
-@Service
-public class MemberServiceImpl implements MemberService,UserDetailsService {
-
-@Autowired
-MemberMapper memberMapper;
-	
-	@Override
-	public MemberVO getMember(String mid) {
-		return memberMapper.getMember(mid); 
-	}
-
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		MemberVO vo = memberMapper.getMember(username);
-		if(vo == null) {
-			throw new UsernameNotFoundException("no name");
-		}
-		return new UserDetailVO(vo);
-	}
-
-}
+//package com.bongsamaru.member.service.impl;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import com.bongsamaru.member.MemberVO;
+//import com.bongsamaru.member.UserDetailVO;
+//import com.bongsamaru.member.mapper.MemberMapper;
+//import com.bongsamaru.member.service.MemberService;
+//
+//@Service
+//public class MemberServiceImpl implements MemberService,UserDetailsService {
+//
+//@Autowired
+//MemberMapper memberMapper;
+//	
+//	@Override
+//	public MemberVO getMember(String mid) {
+//		return memberMapper.getMember(mid); 
+//	}
+//
+//	@Override
+//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		MemberVO vo = memberMapper.getMember(username);
+//		if(vo == null) {
+//			throw new UsernameNotFoundException("no name");
+//		}
+//		return new UserDetailVO(vo);
+//	}
+//
+//}
