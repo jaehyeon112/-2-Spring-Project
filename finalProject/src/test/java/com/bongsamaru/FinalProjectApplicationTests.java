c8a6930eb3a22933ecad87ea9b91fb6d965a3367
hyunaahh <laura3014@naver.com>
2024-01-29 17:53:10 +0900

    0129

--- finalProject/src/test/java/com/bongsamaru/FinalProjectApplicationTests.java
+++ finalProject/src/test/java/com/bongsamaru/FinalProjectApplicationTests.java
@@ -1,15 +1,32 @@
 package com.bongsamaru;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
-import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.bongsamaru.dona.mapper.DonaMapper;
+import com.bongsamaru.dona.service.DonaVO;
+
 @SpringBootTest
-@MapperScan(basePackages = "com.bongsamaru.**.mapper")
+//@MapperScan(basePackages = "com.bongsamaru.**.mapper")
 class FinalProjectApplicationTests {
+	
+	@Autowired
+	DonaMapper donaMapper;
+	
+	
 
-	@Test
-	void contextLoads() {
-	}
-
+   @Test
+   void 상세조회() {
+	   DonaVO donaVO = new DonaVO();
+//	   donaVO.setDonId(1);
+	   DonaVO did = donaMapper.selectDona(1);
+	   System.out.println(did);
+	   //assertThat(did.getTitle(), "제목");
+//	   assertThat(did.isEmpty()).isEqualTo(false); 
+	   
+   }
+   
 }
