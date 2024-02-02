5805dc2586b16a6e266316735c9f2403a8de91ae
yb1012 <tgu02007@naver.com>
2024-02-02 12:41:48 +0900

    일단 중간..

--- finalProject/src/main/java/com/bongsamaru/challenges/web/WebConfig.java
+++ finalProject/src/main/java/com/bongsamaru/challenges/web/WebConfig.java
@@ -1,8 +1,10 @@
 package com.bongsamaru.challenges.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +17,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(facilityInfoInterceptor)
                 .addPathPatterns("/challenge/**");  // "/facilityInfo/**" 경로에만 적용
+    }
+    
+    @Value("${file.loading.path}")
+    String uploadPath;
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+       
+    	// addResourceHandler("매핑 경로")를 적어둔다. localhost:8080/upload/ 로 들어오는 모든 정적 리소스 요청을 static폴더가 아닌 .addResourceLoactions에 적어둔 경로로 부터 찾아준다.
+    	registry.addResourceHandler("/upload/**").addResourceLocations(uploadPath);
     }
 }
--- finalProject/src/main/java/com/bongsamaru/challenges/web/challengesController.java
+++ finalProject/src/main/java/com/bongsamaru/challenges/web/challengesController.java
@@ -1,15 +1,29 @@
 package com.bongsamaru.challenges.web;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.bongsamaru.challenges.mapper.ChallengeMapper;
 import com.bongsamaru.challenges.service.ChallengesService;
 import com.bongsamaru.challenges.vo.ChallengesVO;
 
@@ -18,6 +32,8 @@ public class challengesController {
 	
 	@Autowired
 	ChallengesService challengeService;
+	ChallengeMapper challengeMapper;
+	
 	
 	@GetMapping("/challengeList")
 	public String getChallengeList(Model model) {
@@ -41,7 +57,24 @@ public class challengesController {
 	public String getChallengesList(@RequestParam(name="chalId") Integer chalId, Model model) {
 		List<ChallengesVO> dList = challengeService.getChallengesList(chalId);
 		model.addAttribute("challengesList", dList);
-		System.out.println(dList);
 		return "challenge/challengesList";
 	}
+	@GetMapping("/challenge/challengeInsert")
+	public String insertChallengeForm() {
+		return "/challenge/challengeInsert";
+	}
+	@PostMapping("/challenge/challengeInsert")
+	public int getChallengeInsert(ChallengesVO challengeVO) {
+		int chalId = challengeService.getChallengeInsert(challengeVO);
+		return chalId;
+	}
+	
+	@PostMapping("/challenge/challengesList")
+	@ResponseBody
+	public int getChallengesInsert(ChallengesVO challengeVO) {
+		int chalsId = challengeService.getChallengesInsert(challengeVO);
+		return chalsId;
+	}
+	
+	
 }
