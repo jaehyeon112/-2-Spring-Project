cff9873fcef6ffa63bd7347f752bbbb22fb3a90f
kjh <kjh@naver.com>
2024-02-07 01:50:59 +0900

    asdfasdf

--- finalProject/src/main/java/com/bongsamaru/user/web/CommonUserController.java
+++ finalProject/src/main/java/com/bongsamaru/user/web/CommonUserController.java
@@ -1,19 +1,24 @@
 package com.bongsamaru.user.web;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.bongsamaru.user.service.UserDetailVO;
+
 
 @Controller
 public class CommonUserController {
 	
 	
 	
-	
+	 @GetMapping("/login")
+	    public String loginForm(Model model){
+	        return "login/FacilityLogin";
+	    }
 	
 	
 	
