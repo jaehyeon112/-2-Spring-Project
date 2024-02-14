a41f9857fcb25118a3e0007e2dda983dac965e82
kjh <kjh@naver.com>
2024-02-15 01:36:49 +0900

    asdf

--- finalProject/src/main/java/com/bongsamaru/bongsa/mapper/BongsaMapper.java
+++ finalProject/src/main/java/com/bongsamaru/bongsa/mapper/BongsaMapper.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import com.bongsamaru.common.VO.PageVO;
 import com.bongsamaru.common.VO.VolActVO;
+import com.bongsamaru.common.VO.VolunteerVO;
 
 public interface BongsaMapper {
 	public List<VolActVO> facilityList(PageVO vo);
+	public List<VolunteerVO> volList(PageVO vo);
 	public int cntFacilityList(PageVO vo);
 }
