db1621bd2a4a4deb005ed864c0308edcfc0322b8
kjh <amikjh@naver.com>
2024-02-23 11:24:49 +0900

    ff

--- finalProject/src/main/java/com/bongsamaru/file/web/WebMvcConfig.java
+++ finalProject/src/main/java/com/bongsamaru/file/web/WebMvcConfig.java
@@ -13,6 +13,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
        
     	// addResourceHandler("매핑 경로")를 적어둔다. localhost:8080/upload/ 로 들어오는 모든 정적 리소스 요청을 static폴더가 아닌 .addResourceLoactions에 적어둔 경로로 부터 찾아준다.
-    	registry.addResourceHandler("/upload/**").addResourceLocations("file:///upload/");
+    	registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/upload/");
     }
 }
