2ab45327f2e5a3c307a65e8cd27cb8d14ee4495a
kjh <kjh@naver.com>
2024-03-01 19:41:49 +0900

    전화번호

--- finalProject/src/main/java/com/bongsamaru/mypage/service/sendSmsService.java
+++ finalProject/src/main/java/com/bongsamaru/mypage/service/sendSmsService.java
@@ -9,11 +9,21 @@ import net.nurigo.java_sdk.api.Message;
 import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
 public class sendSmsService {
-
+	@Value("${spring.sms.key}")	
+	private String key;
+	
+	@Value("${spring.sms.secret}")	
+	private String secret;
+	
+	@Value("${spring.sms.number}")	
+	private String number;
+	
+	
 public String PhoneNumberCheck(String to) throws CoolsmsException {
-		
-		String api_key = "NCSIJD69X5DAPRKS";
-		String api_secret = "X5K3P5RRDJXH48YTDEELD85XVFRONZGM";
+	
+	
+		String api_key = key;
+		String api_secret = secret;
 		Message coolsms = new Message(api_key, api_secret);
 		
 		Random rand  = new Random();
@@ -27,7 +37,7 @@ public String PhoneNumberCheck(String to) throws CoolsmsException {
     System.out.println(numStr + "인증번호확인");
 	
 	params.put("to", to); // 수신전화번호 (ajax로 view 화면에서 받아온 값으로 넘김)
-	params.put("from", "01033406795"); // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+	params.put("from", number); // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
 	params.put("type", "sms"); params.put("text", "인증번호는 [" + numStr + "] 입니다.");
 
 
